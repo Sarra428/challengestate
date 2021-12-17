82a27bc9c81e89de6e72525a43ab0c485b1feecb
sarra <sarrah.aouina@gmail.com>
2021-12-17 20:05:55 +0100

    challenge state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+
+  state={
+    todos:[{id:0, task:'eat', done:false},
+           {id:1, task:'sleep', done:false},
+           {id:2, task:'repeat', done:false}],
+
+    text:''
+    
+        }
+
+
+
+        addtodo=()=>{
+
+          this.setState({todos:[...this.state.todos, {id:Math.random(), task:this.state.text, done:false}]    })
+
+        }
+
+
+        deletetodo=(x)=>{
+          this.setState({todos: this.state.todos.filter( el => el.id !=x    )})
+
+        }
+
+
+        donetodo=(x)=>{
+          this.setState({todos:this.state.todos.map( el=>   el.id==x ?  {...el, done:!el.done}   :   el)})
+
+
+        }
+
+
+  render() {
+    return (
+      <div>
+       <input   onChange={(event)=>this.setState({text: event.target.value})}   />
+      <button  onClick={()=> this.addtodo()}>Add</button>
+ 
+      {this.state.todos.map(el=> 
+      <div>
+      <ul>{el.task}</ul>
+      <button  onClick={()=>this.donetodo()}    >Done</button>
+      <button  onClick={ ()=>this.deletetodo()  }    >Delete</button>
+      </div>
+      )}
+
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+export default App
+
